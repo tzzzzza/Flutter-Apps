1ed31613bd58f91a7397bb2643b54977cda168df
tzzzzza <thantzinag282@gmail.com>
2024-05-09 08:53:32 +0630

    initial commit

---
+++ lib/screens/place_detail.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:favorite_places/models/place.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(place.title),
+        ),
+        body: Stack(
+          children: [
+            Image.file(
+              place.image,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ],
+        ));
+  }
+}
